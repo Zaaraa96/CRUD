9d255ea72b313b81b8494712784c82b68abfbca6
zahra ahmadi <zahraahmadi96@gmail.com>
2019-08-27 13:20:43 +0430

    creating the project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+
+        <title></title>
+
+        <!-- Fonts -->
+
+        <!-- Styles -->
+        <style>
+
+        </style>
+    </head>
+    <body>
+
+
+    </body>
+</html>
